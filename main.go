fb09866ded3bac1058e2a31d9d02d994826c7f1d
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-03-30 09:50:02 +1000

    Added repo detail page

--- main.go
+++ main.go
@@ -2,17 +2,67 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"net/http"
 	"text/template"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+
+	"github.com/gorilla/mux"
 )
 
 const (
 	templatesDir = "templates/"
+	staticDir    = "static/"
 )
 
+type RepositoryList struct {
+	Repositories []Repository
+}
+
+type Repository struct {
+	Name string
+}
+
+type RepositoryDetail struct {
+	Name         string
+	TotalCommits int
+}
+
+func repositoryHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	repo, _ := git.PlainOpen(".got/" + vars["repoName"] + "/.git")
+	ref, _ := repo.Head()
+
+	cIter, _ := repo.Log(&git.LogOptions{From: ref.Hash()})
+
+	var cCount int
+	cIter.ForEach(func(c *object.Commit) error {
+		cCount++
+
+		return nil
+	})
+
+	repoDetail := RepositoryDetail{
+		Name:         vars["repoName"],
+		TotalCommits: cCount,
+	}
+
+	templates["repository.html"].Execute(w, repoDetail)
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates["index.html"].Execute(w, nil)
+	repos := RepositoryList{
+		[]Repository{
+			Repository{
+				"go-bencoder2",
+			},
+		},
+	}
+
+	templates["index.html"].Execute(w, repos)
 }
 
 var templates map[string]*template.Template
@@ -26,15 +76,17 @@ func init() {
 	templates = make(map[string]*template.Template, 0)
 
 	for _, f := range files {
-		templates["index.html"] = template.Must(template.ParseFiles(templatesDir + f.Name()))
+		templates[f.Name()] = template.Must(template.ParseFiles(templatesDir + f.Name()))
 	}
 }
 
 func main() {
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	handler := mux.NewRouter()
 
-	http.HandleFunc("/", indexHandler)
+	handler.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	handler.HandleFunc("/repository/{repoName}", repositoryHandler)
+	handler.HandleFunc("/", indexHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
